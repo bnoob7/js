59383e099e2b1b9b5f3d5adc16e1dbc9b0294c2f
bnoob7 <99059282+bnoob7@users.noreply.github.com>
2025-06-07 07:00:00 +0000

    learn datatype and create notes

--- variable/02_datatypes.js
+++ variable/02_datatypes.js
@@ -1,3 +1,25 @@
+/* 
+_____NOTE_____
+
+number => 2 t0 power 53
+BigInt
+string => ""
+boolean => true / false
+null => staldalone value re
+eg{
+    suppose ma chai aauta api bata chai kathmandu ko temperature fetch garxu.BigIntif database bata  fetch vo vane i ll provide the value fetched from the debugger, 
+    yedi fetch nai vako xaina vane chai i ll provide the value null.
+}
+undefined
+Symbol  //especially used in react, extensively usedin figma to seperate the uniqueness..
+ 
+
+*/
+
+
 let name = 'Binod'
 let age = 20
 let address = lalitpur
+
+
+
