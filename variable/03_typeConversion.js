0118763a2c82fb5786ec6c5f6d342d3c997ce1b0
bnoob7 <99059282+bnoob7@users.noreply.github.com>
2025-06-07 07:45:56 +0000

    conversion of datatype

--- variable/03_typeConversion.js
+++ variable/03_typeConversion.js
@@ -5,6 +5,17 @@ let c = null
 
 console.table({score: score, a: a, b: b, c: c});
 
+let convertAtoNumber = Number(a);
+let convertAtoString = String(a);
+let convertAtoNull = null
+
+
+console.table({a: a,
+    a_after_Number_conversion : convertAtoNumber, 
+    a_after_String_conversion: convertAtoString,
+    a_after_Null_conversion : convertAtoNull,
+
+})
 
 
 // console.log(typeof(score));
