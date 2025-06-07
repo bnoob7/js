0f65c5f93c5431c9aa6afa9a35ff1639ff75f4e9
bnoob7 <99059282+bnoob7@users.noreply.github.com>
2025-06-07 05:21:26 +0000

    create variable and difference about them, learn console.table

---
+++ variable/game.js
@@ -0,0 +1,16 @@
+const score  = 100;
+let acocuntName = "binod"
+var acocuntEmail = "binod@gmail.com"
+const accountPassword = "password"
+let justDeclare;  // value will be undefined
+
+
+console.log(score);
+
+
+console.table([score, acocuntName, acocuntEmail, accountPassword, justDeclare]) 
+// intresting schema format ma dekhaudo rixa.
+
+
+// prefer not to use the var 
+// cause it will create unusual problem in block and functional scope
